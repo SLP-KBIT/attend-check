4238f5feb7616993528ce0fd640ebf6cc5d89d0d
Amakuchisan <s18t318@stu.kagawa-u.ac.jp>
2019-11-22 20:15:44 +0900

    名前を表示しないようにした

--- sample.py
+++ sample.py
@@ -8,11 +8,9 @@ def on_connect(tag):
     service_code = 0x1a8b
     sc = nfc.tag.tt3.ServiceCode(service_code >> 6, service_code & 0x3f)
     bc1 = nfc.tag.tt3.BlockCode(0, service=0) # student number
-    bc2 = nfc.tag.tt3.BlockCode(1, service=0) # ﾅﾏｴ
-    data = tag.read_without_encryption([sc], [bc1, bc2])
+    data = tag.read_without_encryption([sc], [bc1])
 
     print ("Student Num: " + data[2:8].decode("utf-8"))
-    print ("Name: " + data[16:32].decode("shift_jis"))
 
 def main():
     with nfc.ContactlessFrontend('usb') as clf:
